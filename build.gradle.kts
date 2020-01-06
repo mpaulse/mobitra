106e5d82f288b76e85140b82273e9da00e7c6e69
Marlon Paulse <mpaulse@gmail.com>
2020-01-06 20:28:49 +0200

    Build: Create the release ZIP file with checksum

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
-import java.io.ByteArrayOutputStream
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import org.gradle.internal.jvm.Jvm
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
+import java.io.FileInputStream
+import java.io.FileWriter
+import java.security.MessageDigest
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 version = "0.2"
 
@@ -122,51 +126,89 @@ tasks {
         into("$buildDir/modules")
     }
 
-    register<Exec>("packageRelease") {
+    register<Task>("buildRelease") {
         dependsOn("copyJpackager", "copyLibs", "copyModules")
-        commandLine(
-            "${Jvm.current().javaHome}/bin/java",
-            "--module-path",
-            "tools/jpackager",
-            "--add-opens",
-            "jdk.jlink/jdk.tools.jlink.internal.packager=jdk.packager",
-            "--module",
-            "jdk.packager/jdk.packager.Main",
-            "create-image",
-            "--input",
-            "$buildDir/libs",
-            "--main-jar",
-            "${project.name}-${project.version}.jar",
-            "--module-path",
-            "$buildDir/modules;tools/jpackager",
-            "--add-modules",
-            "javafx.controls,javafx.fxml",
-            "--strip-native-commands",
-            "--output",
-            "build/release",
-            "--identifier",
-            "com.mpaulse.mobitra",
-            "--name",
-            "Mobitra",
-            "--version",
-            project.version.toString(),
-            "--description",
-            "Mobitra - Telkom Mobile Prepaid LTE Data Usage Tracker",
-            "--icon",
-            "src/main/resources/images/mobitra.ico",
-            "--vendor",
-            "Marlon Paulse",
-            "--copyright",
-            "Copyright (c) ${LocalDate.now().year} Marlon Paulse",
-            "--license-file",
-            "LICENSE.txt",
-            "--singleton",
-            "--verbose")
+        doLast {
+            exec {
+                commandLine(
+                    "${Jvm.current().javaHome}/bin/java",
+                    "--module-path",
+                    "tools/jpackager",
+                    "--add-opens",
+                    "jdk.jlink/jdk.tools.jlink.internal.packager=jdk.packager",
+                    "--module",
+                    "jdk.packager/jdk.packager.Main",
+                    "create-image",
+                    "--input",
+                    "$buildDir/libs",
+                    "--main-jar",
+                    "${project.name}-$version.jar",
+                    "--module-path",
+                    "$buildDir/modules;tools/jpackager",
+                    "--add-modules",
+                    "javafx.controls,javafx.fxml",
+                    "--strip-native-commands",
+                    "--output",
+                    "build/release",
+                    "--identifier",
+                    "com.mpaulse.mobitra",
+                    "--name",
+                    "Mobitra",
+                    "--version",
+                    version.toString(),
+                    "--description",
+                    "Mobitra - Telkom Mobile Prepaid LTE Data Usage Tracker",
+                    "--icon",
+                    "src/main/resources/images/mobitra.ico",
+                    "--vendor",
+                    "Marlon Paulse",
+                    "--copyright",
+                    "Copyright (c) ${LocalDate.now().year} Marlon Paulse",
+                    "--license-file",
+                    "LICENSE.txt",
+                    "--singleton",
+                    "--verbose")
+            }
+            delete {
+                delete("build/release/Mobitra/Mobitra.ico")
+            }
+            copy {
+                from(".") {
+                    include("LICENSE*.txt")
+                }
+                into("build/release/Mobitra")
+            }
+        }
     }
 
-    register<Delete>("release") {
-        dependsOn("packageRelease")
-        delete = setOf("build/release/Mobitra/Mobitra.ico")
+    register<Zip>("zipRelease") {
+        val archive = "Mobitra-${project.version}.zip"
+        archiveFileName.set(archive)
+        destinationDirectory.set(File("build/release"))
+        from("build/release") {
+            include("Mobitra/**")
+        }
+        doLast {
+            val digest = MessageDigest.getInstance("SHA-256")
+            BufferedInputStream(FileInputStream("build/release/$archive")).use { file ->
+                val buf = ByteArray(8192)
+                var n = file.read(buf, 0, 8192)
+                while (n > 0) {
+                    digest.update(buf, 0, n)
+                    n = file.read(buf, 0, 8192)
+                }
+            }
+            val checksum = digest.digest().joinToString(separator = "") { b ->
+                "%02x".format(b)
+            }
+            FileWriter("build/release/$archive.sha256").use { file ->
+                file.write("$checksum\n")
+            }
+        }
+    }
+
+    register<Task>("release") {
+        dependsOn("buildRelease", "zipRelease")
     }
 
     clean {
